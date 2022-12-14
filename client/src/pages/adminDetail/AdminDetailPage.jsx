27d4688899d482d38b236294c82e9978b6028e1d
KyawSwarHtet <kyawswarhtet101094@gmail.com>
2022-12-14 12:53:48 +0630

    changesksh

--- client/src/pages/adminDetail/AdminDetailPage.jsx
+++ client/src/pages/adminDetail/AdminDetailPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import AdminDetail from "../../components/admin/AdminDetail";
-import RequestForm from "../../components/admin/Requestform";
 
 export default function AdmindetailPage() {
   return (
