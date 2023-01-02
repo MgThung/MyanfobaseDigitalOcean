aeb8767250228b0192a9f462a838be7df1088805
KyawSwarHtet <kyawswarhtet101094@gmail.com>
2023-01-02 22:25:07 +0630

    kshdetail

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -80,11 +80,6 @@ export default function Footer() {
                     </Grid>
                   </Box>
                 </Grid>
-<<<<<<< HEAD
-
-=======
-                
->>>>>>> master
                 <Grid item xs={12} sm={6} md={3}>
                   <Box p={0.5}>
                     <h4>Category</h4>
